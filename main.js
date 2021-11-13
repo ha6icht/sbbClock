22484c00c3921078db265274a773480b9e2adbd6
ha6icht <rolf.karlen@gmail.com>
2021-11-13 16:17:46 +0100

    Delete em tag result.push(element)

--- main.js
+++ main.js
@@ -3,26 +3,25 @@
   var clockElements = makeClockElements(
     'b', 'span', 'span', 'em', 'em', 'em', 'em',
     'em', 'em', 'em', 'em', 'em', 'em', 'em',
-    'em', 'i', 'i', 'i'
+    'em', 'i', 'i', 'i', 'i'
   );
 
   function makeClockElements(tags) {
-    var
-      clock = d.getElementById('clock'),
-      result = [];
+    var clock = d.getElementById('clock'), result = [];
     for (var i = 0; tagName = arguments[i]; i++) {
       var element = clock.appendChild(d.createElement(tagName));
-      console.log('var i: ',i)
+      console.log('arguments[i]: ',arguments[i]);
       if (tagName === 'i') result.push(element);
       else if(tagName === 'em'){
+          console.log('var i: ',i)
           if(i>=3&&i<5) element.classList.add('first');
           else if(i>=5&&i<7)  element.classList.add('second');
-          else if(i>=7&&i<9) element.classList.add('thrid');
+          else if(i>=7&&i<9) element.classList.add('third');
           else if(i>=9&&i<11) element.classList.add('fourth');
-          else if(i>11&&i<13) element.classList.add('fifth');
+          else if(i>=11&&i<13) element.classList.add('fifth');
           else if(i>=13&&i<15) element.classList.add('sixth');
           element.classList.add('twelfth');
-          result.push(element);
+          //result.push(element);
       }
     }
     return result;
@@ -30,6 +29,7 @@
 
   function clockUpdate() {
     var now = new Date();
+    console.log('clockElements[0]: ',clockElements[0]);
     clockElements[0].style.transform = 'rotate(' + (
       now.getHours() * 30 + (Math.floor(now.getMinutes() / 12) * 6)
     ) + 'deg)';
